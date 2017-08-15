94b012365492d1bec5f76e6e47e8d4a4f2d1e303
Maxwell Morgan <mjmor@umich.edu>
2017-08-15 01:54:17 +0000

    update update script

--- update_site.sh
+++ update_site.sh
@@ -2,9 +2,15 @@
 
 SITE_ROOT="/var/www/maxwelljmorgan.net/html/"
 PUBLIC_DIR="/home/mjmor/repos/hugo-personal-site/public/"
+HUGO_BIN="/usr/bin/hugo"
 
-if git pull; then
-    /home/mjmor/go/bin/hugo
+REPO_UPDATED=false
+git pull
+if [[ $? -eq 1 ]]; then REPO_UPDATED=true; echo "Website updated in repo"; fi
+OVERRIDE=false
+if [[ $# -eq 1 ]]; then OVERRIDE=true; echo "Website update override set"; fi
+if $REPO_UPDATED || $OVERRIDE; then
+    $HUGO_BIN
     if [ ! -z "$SITE_ROOT" ]; then
         sudo find ${SITE_ROOT} ! -path "*/.well-known*" ! -path \
             ${SITE_ROOT} -type d -name "*" -exec rm -rf {} +
@@ -15,4 +21,6 @@ if git pull; then
         sudo cp -R ${PUBLIC_DIR}* $SITE_ROOT
     fi
     sudo systemctl reload nginx.service
+else
+    echo "No update needed and no override used"; exit 1;
 fi
